1da091609f02dceb0a332dc8aa73de88217a1b20
Ash Isaac <aisaac@mednetstudy.com>
2015-05-20 08:57:11 -0500

    Added App

---
+++ App.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once('src/Cart.php');
+
+class App {
+
+    static function checkout() {
+        $cart = new Cart;
+        $cart->setTaxContext(new StateTaxContext('MN'));
+        $cart->setDiscount(new Discount(5.00));
+        $p1 = new ProductLineItem(9000, 44.99);
+        $p2 = new ProductLineItem(5050, 19.99);
+        $cart->addLineItem($p1->setQuantity(3));
+        $cart->addLineItem($p2);
+        echo "Cart Count: {$cart->getLineItemCount()} Total: {$cart->getTotal()}\n";
+    }
+
+}
+
+App::checkout();
--- src/Cart.php
+++ src/Cart.php
@@ -72,13 +72,3 @@ class Cart {
     }
 
 }
-
-
-$cart = new Cart;
-$cart->setTaxContext(new StateTaxContext('MN'));
-$cart->setDiscount(new Discount(5.00));
-$p1 = new ProductLineItem(9000, 44.99);
-$p2 = new ProductLineItem(5050, 19.99);
-$cart->addLineItem($p1->setQuantity(3));
-$cart->addLineItem($p2);
-//echo "Cart Count: {$cart->getLineItemCount()} Total: {$cart->getTotal()}\n";
